b6fe2dc14d368f3239cba04babf5a072f1e85c68
bdew <blackdew@gmail.com>
2013-12-13 17:25:23 +0200

    Switched to use the token replacement in forgegradle

--- src/net/bdew/lib/BdLib.scala
+++ src/net/bdew/lib/BdLib.scala
@@ -3,9 +3,9 @@ package net.bdew.lib
 import cpw.mods.fml.common.Mod
 import cpw.mods.fml.common.event.FMLPreInitializationEvent
 
-@Mod(modid = "bdlib", version = "@BdLibVer@", modLanguage =  "scala")
+@Mod(modid = "bdlib", name="BD lib", version = "BDLIB_VER", modLanguage =  "scala")
 object BdLib {
   def preInit(ev: FMLPreInitializationEvent) {
-    ev.getModLog.info("bdlib @BdLibVer@ loaded")
+    ev.getModLog.info("bdlib BDLIB_VER loaded")
   }
 }
