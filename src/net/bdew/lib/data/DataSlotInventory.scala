32617ff59259257f9c9c306cd9b9d40b9fd73f8c
bdew <blackdew@gmail.com>
2014-09-02 14:10:50 +0300

    Added DataSlotInventory

---
+++ src/net/bdew/lib/data/DataSlotInventory.scala
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) bdew, 2013 - 2014
+ * https://github.com/bdew/bdlib
+ *
+ * This mod is distributed under the terms of the Minecraft Mod Public
+ * License 1.0, or MMPL. Please check the contents of the license located in
+ * http://bdew.net/minecraft-mod-public-license/
+ */
+
+package net.bdew.lib.data
+
+import net.bdew.lib.Misc
+import net.bdew.lib.data.base.{DataSlot, TileDataSlots, UpdateKind}
+import net.bdew.lib.tile.inventory.BaseInventory
+import net.minecraft.item.ItemStack
+import net.minecraft.nbt.{NBTTagCompound, NBTTagList}
+
+case class DataSlotInventory(name: String, parent: TileDataSlots, size: Int) extends DataSlot with BaseInventory {
+  setUpdate(UpdateKind.SAVE)
+
+  override def getSizeInventory = size
+  override def markDirty() = parent.dataSlotChanged(this)
+
+  override def load(t: NBTTagCompound, kind: UpdateKind.Value) {
+    inv = new Array[ItemStack](getSizeInventory)
+    for (nbtItem <- Misc.iterNbtCompoundList(t, name)) {
+      val slot = nbtItem.getByte("Slot")
+      if (slot >= 0 && slot < inv.size) {
+        inv(slot) = ItemStack.loadItemStackFromNBT(nbtItem)
+      }
+    }
+  }
+
+  override def save(t: NBTTagCompound, kind: UpdateKind.Value) {
+    val itemList = new NBTTagList
+    for ((item, i) <- inv.view.zipWithIndex if item != null) {
+      val itemNbt: NBTTagCompound = new NBTTagCompound
+      itemNbt.setByte("Slot", i.asInstanceOf[Byte])
+      item.writeToNBT(itemNbt)
+      itemList.appendTag(itemNbt)
+    }
+    t.setTag(name, itemList)
+  }
+}
+
+
