b1e0e70fc4eb1c64bf49844bce868ae82225562b
bdew <blackdew@gmail.com>
2014-09-23 14:04:53 +0300

    Added WidgetSimpleIcon

---
+++ src/net/bdew/lib/gui/widgets/WidgetSimpleIcon.scala
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) bdew, 2013 - 2014
+ * https://github.com/bdew/bdlib
+ *
+ * This mod is distributed under the terms of the Minecraft Mod Public
+ * License 1.0, or MMPL. Please check the contents of the license located in
+ * http://bdew.net/minecraft-mod-public-license/
+ */
+
+package net.bdew.lib.gui.widgets
+
+import net.bdew.lib.gui._
+
+case class WidgetSimpleIcon(rect: Rect, icon: Texture) extends Widget {
+  override def draw(mouse: Point) = parent.drawTexture(rect, icon)
+}
