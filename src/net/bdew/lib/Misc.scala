e7787fb9ca2bc7955680cbe1ecc0e4344404c560
bdew <blackdew@gmail.com>
2015-05-24 04:01:36 +0300

    bumped version to 1.8.0, removed deprecated methods

--- src/net/bdew/lib/Misc.scala
+++ src/net/bdew/lib/Misc.scala
@@ -24,18 +24,6 @@ import net.minecraftforge.fluids.{Fluid, FluidStack}
 import net.minecraftforge.oredict.ShapedOreRecipe
 
 object Misc {
-  @deprecated("Use net.bdew.lib.nbt instead", "bdlib 1.7.0")
-  def iterNbtCompoundList(parent: NBTTagCompound, name: String): Iterable[NBTTagCompound] = {
-    import net.bdew.lib.nbt._
-    parent.getList[NBTTagCompound](name)
-  }
-
-  @deprecated("Use net.bdew.lib.nbt instead", "bdlib 1.7.0")
-  def iterNbtIntArray(parent: NBTTagCompound, name: String): Iterable[Array[Int]] = {
-    import net.bdew.lib.nbt._
-    parent.getList[Array[Int]](name)
-  }
-
   def getActiveModId = Loader.instance().activeModContainer().getModId
 
   def toLocal(s: String) = StatCollector.translateToLocal(s)
