4e94a5b37e83447d70fa2390ee78313bec162b64
bdew <blackdew@gmail.com>
2016-01-27 20:06:46 +0200

    Added experimental workaround for TileEntities not being ready on client when rendering

--- src/net/bdew/lib/block/HasTE.scala
+++ src/net/bdew/lib/block/HasTE.scala
@@ -9,11 +9,11 @@
 
 package net.bdew.lib.block
 
-import net.bdew.lib.BdLib
+import net.bdew.lib.{BdLib, Client}
 import net.minecraft.block.{Block, ITileEntityProvider}
 import net.minecraft.tileentity.TileEntity
 import net.minecraft.util.BlockPos
-import net.minecraft.world.{IBlockAccess, World}
+import net.minecraft.world.{ChunkCache, IBlockAccess, World}
 
 trait HasTE[T] extends Block with ITileEntityProvider {
   val TEClass: Class[_ <: TileEntity]
@@ -23,13 +23,17 @@ trait HasTE[T] extends Block with ITileEntityProvider {
   def getTE(w: IBlockAccess, pos: BlockPos): T = {
     var t = w.getTileEntity(pos)
     if ((t == null) || !TEClass.isInstance(t)) {
-      BdLib.logError("Tile entity for block %s at (%d,%d,%d) is corrupt or missing - recreating", this, pos.getX, pos.getY, pos.getZ)
+      BdLib.logWarn("Tile entity for block %s at (%d,%d,%d) is corrupt or missing - recreating", this, pos.getX, pos.getY, pos.getZ)
       w match {
         case ww: World =>
           t = createNewTileEntity(ww, getMetaFromState(w.getBlockState(pos)))
           ww.setTileEntity(pos, t)
+        case ww: ChunkCache =>
+          // This is in client, TE probably didn't arrive from server yet, make a fake one and hope for the best
+          t = TEClass.newInstance()
+          t.setWorldObj(Client.world)
         case _ =>
-          sys.error("Unable to recreate TE")
+          sys.error("Something is broken. Report to mod author. TileEntity type %s for block %s, world is %s".format(TEClass.getName, getRegistryName, w.getClass.getName))
       }
     }
     return t.asInstanceOf[T]
