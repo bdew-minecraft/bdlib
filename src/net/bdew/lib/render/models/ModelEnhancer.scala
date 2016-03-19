3a62b4bb4619a095828aeb24bbdd5803e9f0c7fc
bdew <blackdew@gmail.com>
2016-03-19 15:14:53 +0200

    Added itemOverrideList support to ModelEnhancer

--- src/net/bdew/lib/render/models/ModelEnhancer.scala
+++ src/net/bdew/lib/render/models/ModelEnhancer.scala
@@ -14,7 +14,7 @@ import java.util
 import com.google.common.base.Function
 import com.google.common.collect.ImmutableMap
 import net.minecraft.block.state.IBlockState
-import net.minecraft.client.renderer.block.model.BakedQuad
+import net.minecraft.client.renderer.block.model.{BakedQuad, ItemOverrideList}
 import net.minecraft.client.renderer.texture.TextureAtlasSprite
 import net.minecraft.client.renderer.vertex.VertexFormat
 import net.minecraft.util.{EnumFacing, ResourceLocation}
@@ -50,6 +50,8 @@ abstract class ModelEnhancer {
     * @return wrapped model, that will bake into a ISmartBlockModel with our logic
     */
   def wrap(base: IModel): IRetexturableModel = new SmartUnbakedWrapper(base)
+
+  val itemOverrideList = ItemOverrideList.NONE
 
   /**
     * Combines this with another ModelEnhancer
