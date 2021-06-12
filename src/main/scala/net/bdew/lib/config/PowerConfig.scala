cd07cf4007c64bbc8169f0c12af6cc98326cd276
bdew <blackdew@gmail.com>
2021-06-12 10:38:42 +0300

    Power config values have to be positive

--- src/main/scala/net/bdew/lib/config/PowerConfig.scala
+++ src/main/scala/net/bdew/lib/config/PowerConfig.scala
@@ -3,6 +3,6 @@ package net.bdew.lib.config
 import net.minecraftforge.common.ForgeConfigSpec
 
 class PowerConfig(spec: ForgeConfigSpec.Builder, defMaxReceive: Float, defCapacity: Float) extends ConfigSection {
-  val maxReceive: () => Float = floatVal(spec, "MaxInput", "Maximum energy input (FE/t)", defMaxReceive)
-  val capacity: () => Float = floatVal(spec, "Capacity", "Energy capacity (FE)", defCapacity)
+  val maxReceive: () => Float = floatVal(spec, "MaxInput", "Maximum energy input (FE/t)", defMaxReceive, 0)
+  val capacity: () => Float = floatVal(spec, "Capacity", "Energy capacity (FE)", defCapacity, 0)
 }
