199d701c112adf35a4edab6c1b92e52eb796f979
bdew <blackdew@gmail.com>
2022-02-02 09:08:35 +0200

    BlockController is always ticking on server

--- src/main/scala/net/bdew/lib/multiblock/block/BlockController.scala
+++ src/main/scala/net/bdew/lib/multiblock/block/BlockController.scala
@@ -1,18 +1,18 @@
 package net.bdew.lib.multiblock.block
 
-import net.bdew.lib.block.HasTE
+import net.bdew.lib.block.HasTETickingServer
 import net.bdew.lib.multiblock.tile.TileController
 import net.bdew.lib.render.connected.ConnectedTextureBlock
 import net.minecraft.core.BlockPos
-import net.minecraft.world.{InteractionHand, InteractionResult}
 import net.minecraft.world.entity.player.Player
-import net.minecraft.world.level.{BlockGetter, Level}
 import net.minecraft.world.level.block.Block
 import net.minecraft.world.level.block.state.{BlockBehaviour, BlockState}
+import net.minecraft.world.level.{BlockGetter, Level}
 import net.minecraft.world.phys.BlockHitResult
+import net.minecraft.world.{InteractionHand, InteractionResult}
 
 abstract class BlockController[T <: TileController](props: BlockBehaviour.Properties)
-  extends Block(props) with HasTE[T] with ConnectedTextureBlock {
+  extends Block(props) with HasTETickingServer[T] with ConnectedTextureBlock {
 
   override def onRemove(state: BlockState, world: Level, pos: BlockPos, newState: BlockState, moving: Boolean): Unit = {
     getTE(world, pos).onBreak()
