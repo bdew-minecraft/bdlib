45ebfde9fb2c74e857f3ff57c2fb1a01dc2e3fdb
bdew <blackdew@gmail.com>
2022-02-16 16:46:36 +0200

    reworked BaseContainer.quickMoveStack to use slots when moving from inventory into machine

--- src/main/scala/net/bdew/lib/container/BaseContainer.scala
+++ src/main/scala/net/bdew/lib/container/BaseContainer.scala
@@ -1,24 +1,40 @@
 package net.bdew.lib.container
 
+import net.bdew.lib.Misc
 import net.bdew.lib.items.ItemUtils
 import net.minecraft.world.Container
 import net.minecraft.world.entity.player.Player
 import net.minecraft.world.inventory.MenuType
 import net.minecraft.world.item.ItemStack
 
+import scala.jdk.CollectionConverters._
+
 abstract class BaseContainer(te: Container, containerType: MenuType[_], id: Int) extends NoInvContainer(containerType, id) {
   override def quickMoveStack(player: Player, slotNum: Int): ItemStack = {
-    val slot = getSlot(slotNum)
-    var stack = slot.getItem
+    val sourceSlot = getSlot(slotNum)
+    var sourceStack = sourceSlot.getItem
+
     if (getSlot(slotNum).container == player.getInventory) {
-      stack = ItemUtils.addStackToSlots(stack, te, 0 until te.getContainerSize, true)
+      for (target <- slots.asScala if !sourceStack.isEmpty && target.isActive && !sourceSlot.isSameInventory(target) && target.mayPlace(sourceStack) && ItemStack.isSameItemSameTags(sourceStack, target.getItem)) {
+        val targetStack = target.getItem
+        val toAdd = Misc.min(target.getMaxStackSize - targetStack.getCount, target.container.getMaxStackSize - targetStack.getCount, sourceStack.getCount)
+        if (toAdd > 0) {
+          targetStack.grow(toAdd)
+          sourceStack.shrink(toAdd)
+          target.set(targetStack)
+        }
+      }
+      for (target <- slots.asScala if !sourceStack.isEmpty && target.isActive && !sourceSlot.isSameInventory(target) && target.mayPlace(sourceStack) && target.getItem.isEmpty) {
+        val toAdd = Misc.min(target.getMaxStackSize, target.container.getMaxStackSize, sourceStack.getCount)
+        if (toAdd > 0)
+          target.set(sourceStack.split(toAdd))
+      }
     } else {
-      stack = ItemUtils.addStackToSlots(stack, player.getInventory, 9 until player.getInventory.items.size(), true)
-      if (!stack.isEmpty)
-        stack = ItemUtils.addStackToSlots(stack, player.getInventory, 0 until 9, true)
+      sourceStack = ItemUtils.addStackToSlots(sourceStack, player.getInventory, 9 until player.getInventory.items.size(), true)
+      if (!sourceStack.isEmpty)
+        sourceStack = ItemUtils.addStackToSlots(sourceStack, player.getInventory, 0 until 9, true)
     }
-    slot.set(stack)
-    slot.setChanged()
+    sourceSlot.set(sourceStack)
     ItemStack.EMPTY
   }
 
