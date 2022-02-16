98e15b2c0f3aa3ce1aaf3f421bb6e091c75fc02b
bdew <blackdew@gmail.com>
2022-02-16 16:51:44 +0200

    derpy code cleanup

--- src/main/scala/net/bdew/lib/gui/Direction.scala
+++ src/main/scala/net/bdew/lib/gui/Direction.scala
@@ -1,11 +1,9 @@
 package net.bdew.lib.gui
 
-import net.bdew.lib.gui
-
 object Direction extends Enumeration {
   type Direction = Value
-  val UP: gui.Direction.Value = Value("UP")
-  val DOWN: gui.Direction.Value = Value("DOWN")
-  val LEFT: gui.Direction.Value = Value("LEFT")
-  val RIGHT: gui.Direction.Value = Value("RIGHT")
+  val UP: Value = Value("UP")
+  val DOWN: Value = Value("DOWN")
+  val LEFT: Value = Value("LEFT")
+  val RIGHT: Value = Value("RIGHT")
 }
