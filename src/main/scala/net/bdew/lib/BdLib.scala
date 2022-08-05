d66cc9602597fb01c95e3c921fccc67b2655a9ac
bdew <blackdew@gmail.com>
2022-08-05 11:27:40 +0300

    update SLP and revert mod class back to object

--- src/main/scala/net/bdew/lib/BdLib.scala
+++ src/main/scala/net/bdew/lib/BdLib.scala
@@ -10,9 +10,19 @@ import net.minecraftforge.event.RegisterCommandsEvent
 import net.minecraftforge.fml.common.Mod
 import org.apache.logging.log4j.{LogManager, Logger}
 
-
 @Mod(BdLib.ModId)
-class BdLib {
+object BdLib {
+  final val ModId = "bdlib"
+
+  val log: Logger = LogManager.getLogger
+
+  def logDebug(msg: String, args: Any*): Unit = log.debug(msg.format(args: _*))
+  def logInfo(msg: String, args: Any*): Unit = log.info(msg.format(args: _*))
+  def logWarn(msg: String, args: Any*): Unit = log.warn(msg.format(args: _*))
+  def logError(msg: String, args: Any*): Unit = log.error(msg.format(args: _*))
+  def logWarnException(msg: String, t: Throwable, args: Any*): Unit = log.warn(msg.format(args: _*), t)
+  def logErrorException(msg: String, t: Throwable, args: Any*): Unit = log.error(msg.format(args: _*), t)
+
   MinecraftForge.EVENT_BUS.addListener(this.registerCommands)
 
   LootFunctionTypes.init()
@@ -26,17 +36,4 @@ class BdLib {
     event.getDispatcher.register(CommandOreDistribution.register)
     event.getDispatcher.register(CommandDumpRegistry.register)
   }
-}
-
-object BdLib {
-  final val ModId = "bdlib"
-
-  val log: Logger = LogManager.getLogger
-
-  def logDebug(msg: String, args: Any*): Unit = log.debug(msg.format(args: _*))
-  def logInfo(msg: String, args: Any*): Unit = log.info(msg.format(args: _*))
-  def logWarn(msg: String, args: Any*): Unit = log.warn(msg.format(args: _*))
-  def logError(msg: String, args: Any*): Unit = log.error(msg.format(args: _*))
-  def logWarnException(msg: String, t: Throwable, args: Any*): Unit = log.warn(msg.format(args: _*), t)
-  def logErrorException(msg: String, t: Throwable, args: Any*): Unit = log.error(msg.format(args: _*), t)
 }
